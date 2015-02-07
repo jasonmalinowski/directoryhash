a19468e92d610893d20908ca3b0cc00c6007b383
Jason Malinowski <jason@jason-m.com>
2015-02-06 22:23:25 -0800

    Initial commit of a empty console application

---
+++ DirectoryHash/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryHash
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DirectoryHash/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DirectoryHash")]
+[assembly: AssemblyProduct("DirectoryHash")]
+[assembly: AssemblyCopyright("Copyright © 2015 Jason Malinowski")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
