ff04786c6896fd0063bbe68713fdb69a3a36ad4d
Jason Malinowski <jason@jason-m.com>
2015-11-29 14:09:15 -0800

    Add some tests for the recompute command

---
+++ DirectoryHashTests/RecomputeTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DirectoryHash.Tests
+{
+    public class RecomputeTests
+    {
+        [Fact]
+        public void RecomputeOnEmptyDirectory()
+        {
+            using (var temporaryDirectory = new TemporaryDirectory())
+            {
+                temporaryDirectory.Run("recompute");
+
+                var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
+
+                Assert.Empty(hashes.HashedDirectory.Directories);
+                Assert.Empty(hashes.HashedDirectory.Files);
+            }
+        }
+
+        [Fact]
+        public void RecomputeWithFile()
+        {
+            using (var temporaryDirectory = new TemporaryDirectory())
+            {
+                var file = temporaryDirectory.CreateFileWithContent("Fox", Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog."));
+                temporaryDirectory.Run("recompute");
+
+                var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
+                var rehashedFile = HashedFile.FromFile(file);
+
+                Assert.Equal(rehashedFile, hashes.HashedDirectory.Files["Fox"]);
+            }
+        }
+    }
+}
