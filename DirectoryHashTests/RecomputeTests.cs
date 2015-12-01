c7a1553fdfea4bab68065614a65bd4bc20ac6395
Jason Malinowski <jason@jason-m.com>
2015-11-30 23:33:32 -0800

    Move some common test setup code into the test class constructor

--- DirectoryHashTests/RecomputeTests.cs
+++ DirectoryHashTests/RecomputeTests.cs
@@ -7,49 +7,47 @@ using Xunit;
 
 namespace DirectoryHash.Tests
 {
-    public class RecomputeTests
+    public class RecomputeTests : IDisposable
     {
+        private readonly TemporaryDirectory temporaryDirectory = new TemporaryDirectory();
+
+        void IDisposable.Dispose()
+        {
+            temporaryDirectory.Dispose();
+        }
+
         [Fact]
         public void RecomputeOnEmptyDirectory()
         {
-            using (var temporaryDirectory = new TemporaryDirectory())
-            {
-                temporaryDirectory.Run("recompute");
+            temporaryDirectory.Run("recompute");
 
-                var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
+            var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
 
-                Assert.Empty(hashes.HashedDirectory.Directories);
-                Assert.Empty(hashes.HashedDirectory.Files);
-            }
+            Assert.Empty(hashes.HashedDirectory.Directories);
+            Assert.Empty(hashes.HashedDirectory.Files);
         }
 
         [Fact]
         public void RecomputeUpdatesTimeStamp()
         {
-            using (var temporaryDirectory = new TemporaryDirectory())
-            {
-                var timeRange = DateTimeRange.CreateSurrounding(
-                    () => temporaryDirectory.Run("recompute"));
+            var timeRange = DateTimeRange.CreateSurrounding(
+                () => temporaryDirectory.Run("recompute"));
 
-                var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
+            var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
 
-                timeRange.AssertContains(hashes.UpdateTime);
-            }
+            timeRange.AssertContains(hashes.UpdateTime);
         }
 
         [Fact]
         public void RecomputeWithFile()
         {
-            using (var temporaryDirectory = new TemporaryDirectory())
-            {
-                var file = temporaryDirectory.CreateFileWithContent("Fox", Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog."));
-                temporaryDirectory.Run("recompute");
+            var file = temporaryDirectory.CreateFileWithContent("Fox", Encoding.ASCII.GetBytes("The quick brown fox jumps over the lazy dog."));
+            temporaryDirectory.Run("recompute");
 
-                var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
-                var rehashedFile = HashedFile.FromFile(file);
+            var hashes = HashesXmlFile.ReadFrom(temporaryDirectory.Directory);
+            var rehashedFile = HashedFile.FromFile(file);
 
-                Assert.Equal(rehashedFile, hashes.HashedDirectory.Files["Fox"]);
-            }
+            Assert.Equal(rehashedFile, hashes.HashedDirectory.Files["Fox"]);
         }
     }
 }
